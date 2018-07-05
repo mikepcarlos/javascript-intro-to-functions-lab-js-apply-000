66cfed7b1e240ff98f07a1562747680c395cf08d
Michael Carlos <michaelpcarlos@gmail.com>
2018-07-05 19:45:26 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+const shout = (str) => {
+  return str.toUpperCase();
+};
+
+const whisper = (str) => {
+  return str.toLowerCase();
+};
+
+const logShout = (str) => {
+  console.log(str.toUpperCase());
+};
+
+const logWhisper = (str) => {
+  console.log(str.toLowerCase());
+};
