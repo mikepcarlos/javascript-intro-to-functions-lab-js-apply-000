ec61db984e5454851e6d982d52b5c55e6eabea34
Michael Carlos <michaelpcarlos@gmail.com>
2018-07-05 19:48:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ const logShout = (str) => {
 const logWhisper = (str) => {
   console.log(str.toLowerCase());
 };
+
+const sayHiToGrandma = (str) => {
+  if (str === str.toLowerCase()) {
+    return "I can't hear you!";
+  } else if (str === str.toUpperCase()) {
+    return 'YES INDEED!';
+  } else if (str === 'I love you, Grandma.') {
+    return 'I love you, too.';
+  }
+};
